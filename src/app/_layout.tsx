53f33ff1588a73a4182ba2990bb6271cfbe487cc
theboyofdream <infinityknight024@gmail.com>
2024-06-19 23:38:58 +0530

    bot: saving progress. date- 19/6/2024, time-11:38:58 pm

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        {/* <Stack.Screen name="(tabs)" options={{ headerShown: false }} /> */}
         <Stack.Screen name="+not-found" />
       </Stack>
     </ThemeProvider>
